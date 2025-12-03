4feb9c8da249f2ea9842eedb50b1de20b97da959
Gabriel Pedot Reis <gbrielpedot@gmail.com>
2025-12-03 20:51:06 -0300

    feat: pg

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2025_10_08_232707) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "movies", force: :cascade do |t|
     t.text "description"
     t.datetime "created_at", null: false
