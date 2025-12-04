0c8c84aa4c47f91b3ab5d10cbda508fb4c66d407
Gabriel Pedot Reis <gbrielpedot@gmail.com>
2025-12-03 21:35:46 -0300

    feat: migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_08_232707) do
+ActiveRecord::Schema[8.0].define(version: 2025_12_04_001834) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "movies", force: :cascade do |t|
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
